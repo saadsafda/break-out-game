f2e9397432fa42b1b15976d897292e5f296521ab
saadsafda <muhammadsaadsafdar2005@gmail.com>
2021-05-18 21:36:08 +0500

    amizing break out game

---
+++ src/data.js
@@ -0,0 +1,31 @@
+export default {
+    ballObj: {
+      x: 20,
+      y: 200,
+      dx: 5,
+      dy: 5,
+      rad: 10,
+      speed: 10,
+    },
+    brickObj: {
+      x: 0.5,
+      y: 50,
+      height: 20,
+      with: 10,
+      density: 2,
+      colors: ["red", "teal"],
+    },
+    player: {
+      name: "Saad",
+      lives: 5,
+      score: 0,
+      level: 1,
+    },
+    paddleProps: {
+      height: 10,
+      width: 100,
+      x: 100,
+      color: "orange",
+      borderRadius: [50, 10, 50, 10],
+    },
+  };
